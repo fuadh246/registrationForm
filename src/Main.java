c24e86dc1b4ab3632959e0dc6b15d5476629f0f6
fuadh246 <93026516+fuadh246@users.noreply.github.com>
2022-05-26 00:57:27 -0400

    2nd com

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
+    static JFrame frame;
+    static JPanel info_panel;
+    static JPanel data_panel;
+    static JPanel button_panel;
+    static JLabel name;
+    static JLabel age;
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        frame = new JFrame("Window");
+        frame.setSize(600,600);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+//        info_panel = new JPanel();
+//        frame.add(info_panel);
+//        name = new JLabel("name");
+//        info_panel.add(name);
+//        data_panel = new JPanel();
+//
+//        button_panel = new JPanel();
+//        frame.add(name);
+//        frame.add(data_panel);
+//        frame.add(button_panel);
+        frame.setVisible(true);
     }
 }
